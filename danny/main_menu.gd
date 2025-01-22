441be0ce91d657bbb413c30b9b85473a3fa03139
HappyDie77 <soodenny123@gmail.com>
2025-01-22 19:00:47 +0100

    Main Menu 1

--- danny/main_menu.gd
+++ danny/main_menu.gd
@@ -3,8 +3,9 @@ extends Control
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
 
+
 func _on_settings_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://danny/Settings_menu.tscn")
 
 
 func _on_quit_pressed():
